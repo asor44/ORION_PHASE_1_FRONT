2ced6ad5fed006857cfd5a8ec95c6e8d468ea9a7
epitechBR <benoit.riandiere@epitech.eu>
2023-11-11 23:20:23 +0100

    header fini

--- Orion_front/src/app/asor44/header/header.component.ts
+++ Orion_front/src/app/asor44/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    const icons = this.el.nativeElement.querySelector('.logo_asor');
+    const nav = this.el.nativeElement.querySelector('nav');
+    const links = this.el.nativeElement.querySelectorAll('nav li');
+
+    icons.addEventListener('click', () => {
+      if (nav.classList.contains('active')) {
+        this.renderer.removeClass(nav, 'active');
+
+      } else {
+        this.renderer.addClass(nav, 'active');
+
+      }
+    });
+
+
+    // links.forEach((link) => {
+    //   link.addEventListener('click', () => {
+    //     this.renderer.removeClass(nav, 'active');
+    //   });
+    // });
+  }
 }
+
