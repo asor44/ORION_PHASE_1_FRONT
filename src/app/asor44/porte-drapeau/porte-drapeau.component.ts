70c96125f107509776abc8d91ac45ed23fd9c1cd
kevin andrey <kevin.andrey.pro@gmail.com>
2024-07-24 15:36:05 +0200

    fix component

--- src/app/asor44/porte-drapeau/porte-drapeau.component.ts
+++ src/app/asor44/porte-drapeau/porte-drapeau.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-object',
+  selector: 'app-porte-drapeau',
   templateUrl: './porte-drapeau.component.html',
-  styleUrls: ['./object.porte-drapeau.scss']
+  styleUrls: ['./porte-drapeau.component.scss']
 })
-export class ObjectComponent {
+export class PorteDrapeauComponent {
 
 }
