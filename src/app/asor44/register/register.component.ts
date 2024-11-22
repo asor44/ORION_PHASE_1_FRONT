411771d38e3486076b20e72710f31dc095032091
RousseauFlorian <florian.rousseau@epitech.eu>
2024-11-22 14:25:00 +0100

    feat: add register form linked to api

--- src/app/asor44/register/register.component.ts
+++ src/app/asor44/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  step: number = 1; // Étape actuelle
+  form: any = {
+    firstName : '',
+    lastName: '',
+    birthdate: '',
+    email: '',
+    password: '',
+    address: '',
+    postalCode: '',
+    city: '',
+    country: '',
+    phone: ''
+  };
+
+  nextStep(): void {
+    if (this.step === 1) {
+      this.step = 2;
+    }
+  }
+
+  previousStep(): void {
+    if (this.step === 2) {
+      this.step = 1;
+    }
+  }
+
+  async submitForm(): Promise<void> {
+    try {
+      const request = await this.authService.signUp(this.form);
+      if (request.status === 201) {
+        this.router.navigate(['/login']);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
